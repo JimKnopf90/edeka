de174fab76cdfc592c4fbab7185650df80c962c6
SPIELEWIESE\Timm <timm.reichert@telekom.de>
2017-10-08 13:14:00 +0200

    Response

--- js/carrer.js
+++ js/carrer.js
@@ -1,11 +1,12 @@
-window.onload = function() {
+window.onload = function() {  
     
      document.getElementById('listItem1').onclick = function() {  
          document.getElementById("listItem1").style.backgroundColor = "lightblue"
          document.getElementById("listItem2").style.backgroundColor = "#f2f2f2"
          document.getElementById("listItem3").style.backgroundColor = "white"
-         document.getElementById("listItem4").style.backgroundColor = "#f2f2f2"
+         document.getElementById("listItem4").style.backgroundColor = "#f2f2f2" 
          
+           loadConten('carrerJob01');   
      }
      
      document.getElementById('listItem2').onclick = function() {        
@@ -13,6 +14,8 @@ window.onload = function() {
          document.getElementById("listItem2").style.backgroundColor = "lightblue"
          document.getElementById("listItem3").style.backgroundColor = "white"
          document.getElementById("listItem4").style.backgroundColor = "#f2f2f2"
+         
+          loadConten('carrerJob02'); 
      }
      
      document.getElementById('listItem3').onclick = function() {        
@@ -27,8 +30,20 @@ window.onload = function() {
          document.getElementById("listItem2").style.backgroundColor = "#f2f2f2"
          document.getElementById("listItem3").style.backgroundColor = "white"
          document.getElementById("listItem4").style.backgroundColor = "lightblue"
-     }
-         
+     }  
     
+    function loadConten(data) {
+        var client = new XMLHttpRequest();
+        
+        client.open('GET', '/edeka/templates/edeka/sites/' + data + '.html');        
+        client.onreadystatechange = function() {        
+            document.getElementById('divMainContent').innerHTML =  client.responseText;
+}
+        client.send();       
+    
+    }    
+         
+    loadConten('carrerJob01'); 
+    document.getElementById("listItem1").style.backgroundColor = "lightblue"
     
 }
--- js/events.js
+++ js/events.js
@@ -1,9 +1,13 @@
 window.onload = function() {
            
-    document.getElementById('event01').onclick = function() {        
+    document.getElementById('event01').onclick = function() { 
+        document.getElementById("event01").style.backgroundColor = "lightblue";
+        document.getElementById("event02").style.backgroundColor = "#ecf0f1";
         loadConten('event01');   
 }
-    document.getElementById('event02').onclick = function() {        
+    document.getElementById('event02').onclick = function() {   
+        document.getElementById("event01").style.backgroundColor = "#ecf0f1";
+        document.getElementById("event02").style.backgroundColor = "lightblue";
         loadConten('event02');   
 }
     
@@ -16,7 +20,9 @@ window.onload = function() {
 }
         client.send();       
     
-    }    
+    } 
+    loadConten('event01');
+    document.getElementById("event01").style.backgroundColor = "lightblue";
 }
  
 
